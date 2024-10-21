ad47b05eacddb07e820689ec61f14aa2dab8e9c8
SolshineCode <143902425+SolshineCode@users.noreply.github.com>
2024-10-21 13:34:48 -0700

    Changed method back to save_pretrained but without safetensors
    Changed method back to save_pretrained but without safetensors:
    
    merged_model.save_pretrained(output_path, safe_serialization=False)
    
    Tested successful with llama and llama 3.2 models.

--- dam/merge.py
+++ dam/merge.py
@@ -212,7 +212,7 @@ def merge_models(base_model_id,
     print(f"Total number of trainable parameters: {num_trainable_params}")
 
     print(f"Saving merged model to {output_path}")
-    merged_model.save_pretrained(output_path)
+    merged_model.save_pretrained(output_path, safe_serialization=False)
     tokenizer.save_pretrained(output_path)
 
     fixed_config_path = fix_config(output_path, num_models=len(models), non_linearity=non_linearity, merge_embedding_layers=merge_embedding_layers, merge_layernorms=merge_layernorms, uses_base_model=use_base_model)
